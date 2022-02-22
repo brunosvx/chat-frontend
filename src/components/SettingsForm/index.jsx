50b0ec3d108757984436000f7265c0d150a171f4
Bruno <mitozgamer137@gmail.com>
2022-02-21 23:32:34 -0300

    add change name functionality

--- src/components/SettingsForm/index.jsx
+++ src/components/SettingsForm/index.jsx
@@ -1,11 +1,27 @@
+import { useRef } from 'react';
 
 function SettingsForm() {
 
+  const inputNameRef = useRef(null);
+
+  const username = localStorage.getItem('username');
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const name = inputNameRef.current?.value.trim();
+
+    if(name.length <= 0 || name.length > 20) return alert('Nome deve ter entre 1 e 20 caracteres');
+
+    localStorage.setItem('username', name);
+    alert('Configurações salvas')
+  }
+
     return (
-      <form id="settings">
+      <form id="settings" onSubmit={handleFormSubmit}>
           <div className="setting name">
             <h2>Alterar Nome</h2>
-            <input type="text" placeholder="Digite o Nome" />
+            <input type="text" placeholder="Digite o Nome" ref={inputNameRef} defaultValue={username} />
           </div>
           <div className="setting notifications">
             <h2>Sons de Notificações</h2>
